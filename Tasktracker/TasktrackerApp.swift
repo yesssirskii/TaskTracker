3addc8d84a419b87721b0bb8b5af55d2d4fab580
Yesssirskii <ivanmatejcic1@gmail.com>
2026-09-07 18:25:07 +0200

    Initial Commit

---
+++ Tasktracker/TasktrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasktrackerApp.swift
+//  Tasktracker
+//
+//  Created by Ivan Matejčić on 07.09.2026..
+//
+
+import SwiftUI
+
+@main
+struct TasktrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
